7999615208522a14c1860407bf05646f16685234
nelliott82 <nikkoelliott@gmail.com>
2023-07-19 00:33:51 -0400

    Updated photo url src

--- client/src/components/cards/Photos.jsx
+++ client/src/components/cards/Photos.jsx
@@ -8,7 +8,27 @@ import 'swiper/css/navigation';
 
 import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules';
 
-const photos = new Array(20).fill(0);
+// const photos = new Array(20).fill(0);
+const photos = ['https://i.postimg.cc/yWHNZty9/1.jpg',
+                'https://i.postimg.cc/dhLQFYJM/2.jpg',
+                'https://i.postimg.cc/dDjVm65f/3.jpg',
+                'https://i.postimg.cc/ZW2bKMqW/4.jpg',
+                'https://i.postimg.cc/1fR5WFT3/5.jpg',
+                'https://i.postimg.cc/w7Wxnv8c/6.jpg',
+                'https://i.postimg.cc/ftjDCWfd/7.jpg',
+                'https://i.postimg.cc/FfS9fVfD/8.jpg',
+                'https://i.postimg.cc/k6d7WRK9/9.jpg',
+                'https://i.postimg.cc/PvXhRGSR/10.jpg',
+                'https://i.postimg.cc/NLDQrMMq/11.jpg',
+                'https://i.postimg.cc/9rfCPhR4/12.jpg',
+                'https://i.postimg.cc/7fQDmW0K/13.jpg',
+                'https://i.postimg.cc/HrqmC8S5/14.jpg',
+                'https://i.postimg.cc/bZDPM9Rp/15.jpg',
+                'https://i.postimg.cc/kVWmMMZH/16.jpg',
+                'https://i.postimg.cc/jwKrD0YS/17.jpg',
+                'https://i.postimg.cc/4nfgZg18/18.jpg',
+                'https://i.postimg.cc/hQtW0LjF/19.jpg',
+                'https://i.postimg.cc/5Y3Wv7fq/20.jpg']
 
 export default function Photos({ text }) {
 
@@ -31,7 +51,7 @@ export default function Photos({ text }) {
       >
         {photos.map((x, i) =>
           <SwiperSlide key={i}>
-            <img src={`https://github.com/nelliott82/wedding-invite/blob/main/client/dist/assets/Photos/${i + 1}.jpg`}/>
+            <img src={x}/>
           </SwiperSlide>
         )}
       </Swiper>
